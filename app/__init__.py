421d3c3866fbdc2b17e9e425c4c2cb5c0ba9fd7d
Justin Pauga <paugajt@gmail.com>
2023-10-18 16:31:12 -0700

    Removed commented code

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-# # Avoid circular dependencies byimporting here
-# from app.User import bp as user_bp
-# app.register_blueprint(user_bp, url_prefix='/user')
-
-# from app import routes, models
-
 db = SQLAlchemy()
 migrate = Migrate()
 
