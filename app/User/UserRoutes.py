1a83e33a22d91e5f73c654504894a0ea2f856878
tpauga <tpauga@asu.edu>
2023-10-25 14:28:28 -0700

    Attempted to throw HTTP error handlers

--- app/User/UserRoutes.py
+++ app/User/UserRoutes.py
@@ -1,35 +1,55 @@
-from flask import app, request
+from flask import app, request, abort
 from app.User import bp
 
 @bp.route('/')
 def base():
     return 'This is the base user route'
 
-
-
 @app.route('/create-user', methods=['POST'])
 def create_user():
 	
 	request_data = request.get_json()
-	
-	if request_data:
+	try :
+		if request_data:
+			if 'emailAddress' not in request_data:
+				abort(422)
+			if 'firstName' not in request_data:
+				abort(422)
+			if 'lastName' not in request_data:
+				abort(422)
+			if 'password' not in request_data:
+				abort(422)
+			if not isinstance(request_data['emailAddress'],str):
+				abort(400)
+			if not isinstance(request_data['firstName'],str):
+				abort(400)
+			if not isinstance(request_data['lastName'],str):
+				abort(400)
+			if not isinstance(request_data['password'],str):
+				abort(400)
+			
+			emailAddress = request_data['emailAddress']
+			firstName = request_data['firstName']
+			lastName = request_data['lastName']
+			password = request_data['password']
 		
-		if 'emailAddress' in request_data:
-			if isinstance(request_data['emailAddress'],str):
-				emailAddress = request_data['emailAddress']
-		if 'firstName' in request_data:
-			if isinstance(request_data['firstName'],str):
-				firstName = request_data['firstName']
-		if 'lastName' in request_data:
-			if isinstance(request_data['lastName'],str):
-				lastName = request_data['lastName']
-		if 'password' in request_data:
-			if isinstance(request_data['password'],str):
-				password = request_data['password']
+		return '''
+				Email Address = {}
+				First Name = {}
+				Last Name = {}
+				Password = {}
+		'''.format(emailAddress,firstName,lastName,password)
 	
-	return '''
-			Email Address = {}
-			First Name = {}
-			Last Name = {}
-			Password = {}
-	'''.format(emailAddress,firstName,lastName,password)
+
+	except:
+		return '''
+			There was an exception.
+		'''
+
+@app.errorhandler(400)
+def handle_bad_request(error):
+    return 'Bad Request: Wrong data type found in the form.', 400
+
+@app.errorhandler(422)
+def handle_unprocessable_entity(error):
+    return 'Bad Request: No information found in the form.', 400
