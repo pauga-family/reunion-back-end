8dfbf34d5c030b2c64cd3706eeccb4f435021321
tpauga <tpauga@asu.edu>
2023-10-25 13:11:57 -0700

    Erased comments

--- app/routes.py
+++ app/routes.py
@@ -5,19 +5,13 @@ from app import app
 def inded():
 	return "Hello, World!"
 
-# "methods = post" will allow for only POST requests
 @app.route('/create-user', methods=['POST'])
 def create_user():
-	emailAddress = None
-	firstName = None
-	lastName = None
-	password = None
-	# processes JSON data and converts to variables
+	
 	request_data = request.get_json()
-	# check if variables exist in JSON request
+	
 	if request_data:
-		#isinstance(data,dataType) returns True if data is of type dataType, False if else
-		#each if case checks if the data is in JSON request, and then checks if it is correct data type
+		
 		if 'emailAddress' in request_data:
 			if isinstance(request_data['emailAddress'],str):
 				emailAddress = request_data['emailAddress']
@@ -30,7 +24,7 @@ def create_user():
 		if 'password' in request_data:
 			if isinstance(request_data['password'],str):
 				password = request_data['password']
-	#returns information sent from user
+	
 	return '''
 			Email Address = {}
 			First Name = {}
