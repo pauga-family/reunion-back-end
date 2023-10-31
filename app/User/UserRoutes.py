fb9ba636f0d9f6ff18a9c8a321f6fabe4095ef91
tpauga <tpauga@asu.edu>
2023-10-31 03:22:11 -0700

    Immplemented all the changes from code review

--- app/User/UserRoutes.py
+++ app/User/UserRoutes.py
@@ -1,55 +1,43 @@
-from flask import app, request, abort
-from app.User import bp
+import json
+from flask import Response, app, request, abort
+from app.User import UserService, bp
 
 @bp.route('/')
 def base():
     return 'This is the base user route'
 
-@app.route('/create-user', methods=['POST'])
+@bp.route('/create-user', methods=['POST'])
 def create_user():
 	
 	request_data = request.get_json()
-	try :
-		if request_data:
-			if 'emailAddress' not in request_data:
-				abort(422)
-			if 'firstName' not in request_data:
-				abort(422)
-			if 'lastName' not in request_data:
-				abort(422)
-			if 'password' not in request_data:
-				abort(422)
-			if not isinstance(request_data['emailAddress'],str):
-				abort(400)
-			if not isinstance(request_data['firstName'],str):
-				abort(400)
-			if not isinstance(request_data['lastName'],str):
-				abort(400)
-			if not isinstance(request_data['password'],str):
-				abort(400)
-			
-			emailAddress = request_data['emailAddress']
+	response = Response(mimetype='application/json')
+
+	if request_data:
+		try:
+			email = request_data['email']
 			firstName = request_data['firstName']
 			lastName = request_data['lastName']
 			password = request_data['password']
 		
-		return '''
-				Email Address = {}
-				First Name = {}
-				Last Name = {}
-				Password = {}
-		'''.format(emailAddress,firstName,lastName,password)
+			if not isinstance(email, str) or not isinstance(firstName, str) or not isinstance(lastName, str) or not isinstance(password, str):
+				response.status = 400
+				response.response = create_failure_json("Invalid data")
+
+				abort(response)
+		except:
+			response.status = 442
+			response.response = create_failure_json("Missing data")
+			abort(response)
+		return UserService.create_user(firstName=firstName, lastName=lastName, email=email, password=password)
 	
+def create_failure_json(message):
+	return json.dumps({"messag_key": message})
 
-	except:
-		return '''
-			There was an exception.
-		'''
 
-@app.errorhandler(400)
+@bp.errorhandler(400)
 def handle_bad_request(error):
     return 'Bad Request: Wrong data type found in the form.', 400
 
-@app.errorhandler(422)
+@bp.errorhandler(422)
 def handle_unprocessable_entity(error):
     return 'Bad Request: No information found in the form.', 400
