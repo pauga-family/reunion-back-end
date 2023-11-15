7b174df2f7156d6d933d0f07ba88865273d359e1
Justin Pauga <paugajt@gmail.com>
2023-11-14 18:40:26 -0800

    Fixed flask environment file, added jwt support, created auth package and moved user login and creation endpoints. Updated user routes to get a user and delete a user.

--- app/User/UserRoutes.py
+++ app/User/UserRoutes.py
@@ -1,41 +1,21 @@
-from app.User import bp
-from flask import request, abort, jsonify, Response
+from app.user import bp
+from flask import request, jsonify, Response
 from .UserService import UserService
+from flask_jwt_extended import jwt_required
 
 userService = UserService()
 
-@bp.route('/')
-def base():
-    return 'This is the base user route'
-
-@bp.route('/create-user', methods=['POST'])
-def createUser():
-    request_data = request.get_json()
-    
-    if request_data:
-        try:
-            email = request_data['email']
-            firstName = request_data['firstName']
-            lastName = request_data['lastName']
-            password = request_data['password']
-
-            if not isinstance(email, str) or not isinstance(firstName, str) or not isinstance(lastName, str) or not isinstance(password, str):
-                return _create_failure_json("Invalid data"), 400
-
-        except:
-            return _create_failure_json("Missing data"), 422
-        
-        user = userService.create_user(firstName=firstName, lastName=lastName, email=email, password=password)
-        return _create_user_json(user)
-
-@bp.route('<int:user_id>', methods=['GET'])        
+# Secure routes
+@bp.route('<int:user_id>', methods=['GET']) 
+@jwt_required()
 def getUser(user_id):
-    user = userService.get_user(user_id)
+    user = userService.get_user_by_id(user_id)
     if user:
-        return _create_user_json(user)
+        return _create_user_json(user, "")
     return Response(status=400)
 
 @bp.route('/delete/<int:user_id>', methods=['DELETE'])
+@jwt_required()
 def deleteUser(user_id):
     if userService.delete_user(user_id):
         return Response(status=200)
@@ -45,9 +25,10 @@ def deleteUser(user_id):
 def _create_failure_json(message):
     return jsonify({"message_key": message})
 
-def _create_user_json(user):
+def _create_user_json(user, token):
     return jsonify(
         {
+            "access_token": token,
             "id": user.id, 
             "firstName": user.firstName,
             "lastName": user.lastName,
