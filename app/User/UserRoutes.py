6ec8dbeab46b7af5b54cf7f3ee18785af1ea3081
stspauga <116019616+stspauga@users.noreply.github.com>
2023-11-17 13:18:34 -0700

    tp_TAS-34 Created User Route (#3)
    * Created User Route
    
    * Created User Route
    
    * Created User Route
    
    * Created User Route
    
    * Erased comments
    
    * Attempted to throw HTTP error handlers
    
    * Immplemented all the changes from code review
    
    * Added the _ in front of create_failure_json() calls
    
    * Changes
    
    * Changed the return statement
    
    ---------
    
    Co-authored-by: tpauga <tpauga@asu.edu>

--- app/User/UserRoutes.py
+++ app/User/UserRoutes.py
@@ -1,5 +1,37 @@
-from app.User import bp
+import json
+from flask import Response, app, request, abort
+from app.User import UserModel, UserService, bp
 
 @bp.route('/')
 def base():
     return 'This is the base user route'
+
+@bp.route('/create-user', methods=['POST'])
+def create_user():
+	
+	request_data = request.get_json()
+	response = Response(mimetype='application/json')
+
+	if request_data:
+		try:
+			email = request_data['email']
+			firstName = request_data['firstName']
+			lastName = request_data['lastName']
+			password = request_data['password']
+		
+			if not isinstance(email, str) or not isinstance(firstName, str) or not isinstance(lastName, str) or not isinstance(password, str):
+				response.status = 400
+				response.response = _create_failure_json("Invalid data")
+
+				abort(response)
+		except:
+			response.status = 442
+			response.response = _create_failure_json("Missing data")
+			abort(response)
+
+		user_instance = UserModel(firstName=firstName, lastName=lastName, email=email, password=password)
+		
+		return json.dumps(user_instance)
+	
+def _create_failure_json(message):
+	return json.dumps({"messag_key": message})
