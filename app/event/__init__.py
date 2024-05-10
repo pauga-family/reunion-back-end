eb74c8f5efd431c2b6a872d724c2f1a363e3eb4d
d-diplodocus <dezirae.choi@gmail.com>
2024-05-09 19:15:06 -0600

    fixed line 5 from event folder __init__.py

--- app/event/__init__.py
+++ app/event/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('event', __name__)
 
-from app.user import event_service, event_model, event_routes
+from app.event import event_service, event_model, event_routes
